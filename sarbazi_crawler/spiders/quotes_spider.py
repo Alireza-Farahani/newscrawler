e4171417a5def6ae54dc9fe44875c0aaa3301408
Alireza Farahani <ar.d.farahani@gmail.com>
2019-11-02 01:04:06 +0330

    accord box tutorial #10

--- sarbazi_crawler/spiders/quotes_spider.py
+++ sarbazi_crawler/spiders/quotes_spider.py
@@ -8,5 +8,16 @@ class QuotesSpiderSpider(scrapy.Spider):
     start_urls = ['http://quotes.toscrape.com/']
 
     def parse(self, response):
-        quotes = response.xpath("//div[@class='quote']//span[@class='text']/text()").extract()
-        yield {'quotes': quotes}
+        quotes = response.xpath("//div[@class='quote']")
+        for quote in quotes:
+            text = quote.xpath(
+                ".//span[@class='text']/text()").extract_first()
+            author = quote.xpath(
+                ".//small//text()").extract_first()
+            yield {'quote': text, "author": author}
+
+        next_page_url = response.xpath("//li[@class='next']//a/@href").extract_first()
+        if next_page_url:
+            absolute_next_page_url = response.urljoin(next_page_url)
+            yield scrapy.Request(absolute_next_page_url)
+
