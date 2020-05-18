bda048a9fb909ab24c50f6d3519121c9b74791e8
Alireza Farahani <ar.d.farahani@gmail.com>
2020-05-18 21:00:58 +0430

    rename res folder to resources

--- definitions.py
+++ definitions.py
@@ -2,4 +2,4 @@ import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 TEST_DIR = os.path.join(ROOT_DIR, 'tests')
-TEST_RESOURCES_DIR = os.path.join(TEST_DIR, 'res')
+TEST_RESOURCES_DIR = os.path.join(TEST_DIR, 'resources')
